87c91ed6f4d7721169b8d4336267fbd8d5dc1d5e
SimzMapisa <simbarashe795@gmail.com>
2023-05-26 17:23:02 +0200

    add home page components

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import About from '@/components/Home/About';
 import Hero from '@/components/Home/Hero';
+import Services from '@/components/Home/Services';
 import RecentWork from '@/components/Home/RecentWork';
 import Image from 'next/image';
 import React from 'react';
+import TechStack from '@/components/Home/TechStack';
 
 const Home = () => {
 	return (
@@ -10,6 +12,8 @@ const Home = () => {
 			<Hero />
 			<RecentWork />
 			<About />
+			<Services />
+			<TechStack />
 		</>
 	);
 };
