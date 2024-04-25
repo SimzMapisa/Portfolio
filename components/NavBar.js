36c8592563d3e40158a6ac9c8f1f0e94a0ce0648
SimzMapisa <simbarashe795@gmail.com>
2024-04-25 13:46:53 +0100

    change navigation layout and add a download resume button, remove the social media links

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,57 +8,47 @@ import Image from "next/image";
 const NavBar = () => {
   const iconSize = "1.2rem";
   return (
-    <div className="w-full flex items-center  h-20 shadow-md shadow-slate-200">
+    <div className="fixed z-50 bg-white w-full flex items-center  h-20 shadow-md shadow-slate-200">
       <nav className="flex flex-1 justify-between max-w-6xl mx-auto px-4">
         <div className="flex items-center">
           <Link href="/" className="font-extrabold text-2xl text-blue-900	">
             <Image
               src="/images/simbacoderoar-logo.png"
-              width={48}
-              height={48}
+              width={60}
+              height={60}
             />
           </Link>
         </div>
         <div className="md:flex md:items-center hidden">
           <ul className="flex items-center">
-            <li className="mx-4 font-semibold text-slate-600 lg:hover:text-blue-950 lg:active:text-blue-950">
+            <li className="mx-4 text-sm font-normal text-slate-700 lg:hover:text-blue-950 lg:active:text-blue-950">
               <Link href="/">Home</Link>
             </li>
-            <li className="mx-4 font-semibold text-slate-600 lg:hover:text-blue-950 lg:active:text-blue-950">
-              <Link href="/portfolio">My Work</Link>
+            <li className="mx-4 text-sm font-normal text-slate-700 lg:hover:text-blue-950 lg:active:text-blue-950">
+              <Link href="/portfolio">Projects</Link>
             </li>
-            <li className="mx-4 font-semibold text-slate-600 lg:hover:text-blue-950 lg:active:text-blue-950">
+            <li className="mx-4 text-sm font-normal text-slate-700 lg:hover:text-blue-950 lg:active:text-blue-950">
+              <Link href="/blog">Blog</Link>
+            </li>
+            <li className="mx-4 text-sm font-normal text-slate-700 lg:hover:text-blue-950 lg:active:text-blue-950">
               <Link href="/contact">Contact</Link>
             </li>
-            <Button
-              classes="mx-4 font-semibold border-2 border-blue-900 text-blue-900 px-6 py-3 rounded-md"
-              title="Resume"
-            />
           </ul>
         </div>
         <div className="md:flex md:items-center hidden">
-          <ul className="flex items-center">
-            <li className="mx-2 text-slate-500 lg:hover:text-blue-950">
-              <Link href="">
-                <FaLinkedinIn size={iconSize} />
-              </Link>
-            </li>
-            <li className="mx-2 text-slate-500 lg:hover:text-blue-950">
-              <Link href="">
-                <FaGithub size={iconSize} />
-              </Link>
-            </li>
-            <li className="mx-2 text-slate-500 lg:hover:text-blue-950">
-              <Link href="">
-                <FaTwitter size={iconSize} />
-              </Link>
-            </li>
-            <li className="mx-2 text-slate-500 lg:hover:text-blue-950">
-              <Link href="">
-                <GrInstagram size={iconSize} />
-              </Link>
-            </li>
-          </ul>
+          {/* <Button
+            classes="mx-4 font-semibold border-2 border-blue-900 text-blue-900 px-6 py-3 rounded-md"
+            title="Resume"
+          /> */}
+          <Link
+            className="px-5 py-3 rounded-lg bg-violet-700 flex text-white"
+            href="/portfolio"
+          >
+            Download Resume{" "}
+            <span className="pl-3 text-2xl">
+              <Image src="/images/download.svg" width={20} height={0} />
+            </span>
+          </Link>
         </div>
       </nav>
     </div>
