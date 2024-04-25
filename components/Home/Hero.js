22199ebf590d283c8a0a9df39aa4120610693b85
SimzMapisa <simbarashe795@gmail.com>
2024-04-25 17:51:59 +0100

    Changed the the layout and content on the hero section

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <>
-      <div className="justify-center flex">
+      <div className="justify-center flex pt-12">
         <Image
           className="absolute mt-36 z-0"
           src="/Elements.svg"
@@ -15,19 +15,16 @@ const Hero = () => {
           alt="background design elements"
         />
         <div className="relative px-4 z-10 py-20 lg:py-40 md:grid md:grid-cols-12 max-w-6xl mx-auto">
-          <div className=" lg:order-1 md:order-2 md:col-span-12 lg:col-span-6 mb-6">
-            <h1 className="text-3xl md:text-4xl lg:text-4xl text-slate-800 font-extrabold pb-4">
-              I design and develop good looking websites and apps that actually
-              work.
+          <div className=" lg:order-1 md:order-2 md:col-span-12 lg:col-span-6 mb-6 lg:pr-6">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl text-slate-800 font-extrabold pb-4">
+              Crafting stunning websites and apps that simply work.
             </h1>
-            <p className="leading-7 font-medium text-slate-500 mt-1 mb-8">
-              I specialize in designing and developing visually stunning
-              websites and apps that deliver seamless functionality and
-              exceptional user experiences. From elegant websites to
-              cutting-edge mobile applications, my work brings your vision to
-              life, ensuring it not only looks good but also works flawlessly.
-              Experience the perfect blend of aesthetics and functionality for
-              your digital needs.
+            <p className=" font-normal text-base text-slate-700 mt-1 mb-8">
+              From streamlining e-commerce checkouts to enhancing user
+              experiences, we specialize in crafting stunning websites and apps
+              that not only captivate but seamlessly function. My focus?
+              Ensuring your digital presence not only dazzles but delivers
+              results.
             </p>
             {/* <Button
 							classes='bg-blue-900 text-white text-lg mt-5 px-8 py-3 rounded-md'
@@ -35,13 +32,15 @@ const Hero = () => {
 						/> */}
 
             <Link
-              className="px-8 py-4 rounded-lg bg-blue-950 text-white"
+              className="px-5 py-3 rounded-lg bg-violet-700 text-white"
               href="/portfolio"
+              role="button"
             >
-              My Work
+              <span>Projects</span>
+              <span className="pl-3 text-2xl">&#8594;</span>
             </Link>
           </div>
-          <div className="flex lg:order-2 md:order-1 justify-center md:col-span-12 lg:col-span-6">
+          <div className="flex  lg:order-2 md:order-1 justify-center md:col-span-12 lg:col-span-6">
             <Image
               src="/simbarashe-mapisa-hero-img.svg"
               alt="code describing simbas skills"
