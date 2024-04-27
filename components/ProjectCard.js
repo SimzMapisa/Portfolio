2ce84e83804c8dd0a10bed47ea8155809ac6de09
SimzMapisa <simbarashe795@gmail.com>
2024-04-27 20:49:18 +0100

    changes product card layout

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,18 +1,31 @@
 import Image from "next/image";
 import React from "react";
 
-const ProjectCard = ({ title, techStack, type, imgUrl, margin }) => {
-  console.log(title, techStack);
+const ProjectCard = ({ title, content, techStack, imgUrl }) => {
   return (
-    <div className={margin}>
-      <div className="bg-white shadow-lg shadow-slate-100 min-h-full pb-4 rounded-lg">
-        <div className="mb-4">
-          <Image src={imgUrl} width="400" height="300" alt={title + "image"} />
+    <div className=" lg:w-[100%] flex my-4 border-2 rounded-lg overflow-hidden border-violet-700">
+      <div className=" overflow-hidden w-[50%]">
+        <Image
+          src="/images/bg.png"
+          width={300}
+          height={400}
+          className="object-cover scale-150"
+          alt={title}
+        />
+      </div>
+      <div className="py-5 px-7 flex max-w-[60%] flex-col justify-center">
+        <div>
+          <h3 className="text-lg font-semibold pb-4">{title}</h3>
+          <p>{content}</p>
         </div>
-        <div className="pl-4">
-          <h2 className="font-bold text-blue-900">{title}</h2>
-          <h4 className="text-slate-700 font-medium">{type}</h4>
-          <p className="text-xs pt-4 text-slate-600">{techStack}</p>
+        <div className="flex flex-wrap  mt-4 py-4 border-t-2 border-slate-200">
+          {techStack.map((stack) => {
+            return (
+              <p className="mr-2 mb-2 flex items-center justify-center font-normal text-sm text-white px-3 py-1 rounded-full bg-violet-700">
+                {stack}
+              </p>
+            );
+          })}
         </div>
       </div>
     </div>
