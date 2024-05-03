7fc380c12357bb35c753a679460c4188fdc1c02f
SimzMapisa <simbarashe795@gmail.com>
2024-05-03 19:43:16 +0100

    change paragraph and heading styliing

--- components/Home/About.js
+++ components/Home/About.js
@@ -17,17 +17,17 @@ const About = () => {
         </div>
         <div className="flex-1 order-1 lg:order-2 top-0 h-full lg:py-24 ">
           <div className="">
-            <h4 className="text-slate-200 font-bold text-4xl relative z-10">
+            <h4 className="text-white font-bold text-4xl relative z-10">
               About Me
             </h4>
-            <p className="mt-4 text-slate-300 relative z-10 leading-7">
+            <p className="mt-4 text-indigo-50 relative z-10 leading-7">
               The purpose of lorem ipsum is to create a natural looking block of
               text (sentence, paragraph, page, etc.) that doesn&apos;t distract
               from the layout. A practice not without controversy, laying out
               pages with meaningless filler text can be very useful when the
               focus is meant to be on design, not content.
             </p>
-            <p className="mt-4 text-slate-300 leading-7">
+            <p className="mt-4 text-indigo-50 leading-7">
               The purpose of lorem ipsum is to create a natural looking block of
               text (sentence, paragraph, page, etc.) that doesn&apos;t distract
               from the layout. A practice not without controversy, laying out
